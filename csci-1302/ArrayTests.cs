d7b221f0a5cc241c6b617254022e7aebe71ec918
Peter Mourfield <peter.mourfield@gmail.com>
2021-02-15 18:06:36 -0500

    array notes from class

--- csci-1302/ArrayTests.cs
+++ csci-1302/ArrayTests.cs
@@ -8,7 +8,7 @@ namespace csci_1302
     {
         public static void Run()
         {
-            int[] anArray = new int[10];
+            int[] anArray = new int[11];
             //int[] anArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             for(int i = 0; i < anArray.Length; i++)
@@ -22,6 +22,49 @@ namespace csci_1302
             {
                 Console.WriteLine(anArray[j]);
             }
+
+            int someNumber = 14;
+            // int result = LinearSearch(anArray, someNumber);
+            int result = BinarySearch(anArray, someNumber);
+            Console.WriteLine(string.Format("Found {0} at position {1}", someNumber, result));
+        }
+
+        // comparison (==) 1/10,000
+        // 142,000 items
+        // 142,000 / 10,000 = 14.2 worst case 
+        public static int LinearSearch(int[] anArray, int valueToSearchFor)
+        {
+            for (int k = 0; k < anArray.Length; k++)
+            {
+                if (valueToSearchFor == anArray[k])
+                    return k; // return index where found
+            }
+            return -1; // not found case
+        }
+
+        // IMPORTANT: BinarySearch MUST BE SORTED!
+        // int[] anArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        // comparison (==) 1/10,000
+        // 142,000 items
+        // worst case LOG n - 17 searchs        
+        // 17 / 10,000 = 
+        public static int BinarySearch(int[] anArray, int valueToSearchFor)
+        {
+            int mid, first = 0, last = anArray.Length - 1;
+
+            while (first <= last)
+            {
+                mid = (first + last) / 2;
+
+                if (valueToSearchFor > anArray[mid]) // search right half
+                    first = mid + 1;
+                else if (valueToSearchFor < anArray[mid]) // search left half
+                    last = mid - 1;
+                else
+                    return mid;
+            }
+
+            return -1; // not found case
         }
     }
 }
--- csci-1302/Program.cs
+++ csci-1302/Program.cs
@@ -10,6 +10,19 @@ namespace csci_1302
             // ClassPropertiesTests.Run();
             //ExceptionTests.Run();
             ArrayTests.Run();
+
+            //var result = AddTwoNumbers(uint.MaxValue-1, 5);
+            //Console.WriteLine(result);
+
+        }
+        public static uint AddTwoNumbers(uint num1, uint num2)
+        {
+            uint result = num1 + num2;
+
+            if (result < num1 || result < num2)
+                throw new ArgumentOutOfRangeException("Number are hard");
+            
+            return result;
         }
     }
 }
