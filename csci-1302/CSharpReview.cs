f921f613176deb744641a1e79811d6438e115443
Peter Mourfield <peter.mourfield@gmail.com>
2021-02-22 12:35:49 -0500

    Create CSharpReview.cs

---
+++ csci-1302/CSharpReview.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace csci_1302
+{
+    public class CSharpReview
+    {
+        public static void Run()
+        {
+            // initialize loop condition
+            int i = 0;
+            // loop condition
+            while(i < 10)
+            {
+                // do some work
+                Console.WriteLine(string.Format("i = {0}", i));
+                // update loop condition
+                i++;
+            }
+
+            for(int j = 0; j < 10; j++)
+            {
+                Console.WriteLine(string.Format("j = {0}", j));
+            }
+        }
+    }
+}
