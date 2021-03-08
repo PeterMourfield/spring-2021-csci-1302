0146985f93aab0f02a26e406783d9ed93386d39d
Peter Mourfield <peter.mourfield@gmail.com>
2021-03-08 15:13:40 -0500

    Add GenericList

---
+++ csci-1302/GenericList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csci_1302
+{
+    // type parameter T in angle brackets
+    public class GenericList<T>
+    {
+       #region HideThis
+        // The nested class is also generic on T.
+        private class Node
+        {
+            // T used in non-generic constructor.
+            public Node(T t)
+            {
+                next = null;
+                data = t;
+            }
+
+            private Node next;
+            public Node Next
+            {
+                get { return next; }
+                set { next = value; }
+            }
+
+            // T as private member data type.
+            private T data;
+
+            // T as return type of property.
+            public T Data
+            {
+                get { return data; }
+                set { data = value; }
+            }
+        }
+
+        private Node head;
+
+        public bool IsEmpty
+        {
+            get { return head == null;  }
+        }
+
+        // constructor
+        public GenericList()
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            head = null;
+        }
+
+        // T as method parameter type:
+        public void AddHead(T t)
+        {
+            Node n = new Node(t);
+            n.Next = head;
+            head = n;
+        }
+
+        public void AddTail(T t)
+        {
+            if (IsEmpty)
+                AddHead(t);
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                Node n = new Node(t);
+                current.Next = n;
+            }
+        }
+
+        public void RemoveHead()
+        {
+            if (!IsEmpty)
+                head = head.Next;
+        }
+
+        // this method allows us to use foreach
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+        #endregion
+    }
+}
--- csci-1302/Program.cs
+++ csci-1302/Program.cs
@@ -6,12 +6,30 @@ namespace csci_1302
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            // ClassPropertiesTests.Run();
+            //Console.WriteLine("Hello World!");
+            //ClassPropertiesTests.Run();
             //ExceptionTests.Run();
             //Homework2.Run();
-            // ArrayTests.Run();
-            ArrayTests2D.Run();
+            //ArrayTests.Run();
+            //ArrayTests2D.Run();
+
+            #region Lists
+
+            //GenericList<int> theList = new GenericList<int>();
+            //theList.AddHead(1);
+            //theList.AddHead(2);
+            //theList.AddHead(3);
+            //theList.AddTail(1);
+            //theList.AddTail(2);
+            //theList.AddTail(3);
+            GenericList<Person> theList = new GenericList<Person>();
+            theList.AddHead(new Person("Sam", "Smith", 55, 4));
+            theList.AddTail(new Person("Sally", "Smith", 35, 7));
+            foreach (var item in theList)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
         }
     }
 }
