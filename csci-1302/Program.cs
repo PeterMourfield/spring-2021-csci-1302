eaa123bb77799b285a5434551de731430d25f04b
Peter Mourfield <1678724+peter-mourfield@users.noreply.github.com>
2021-03-15 15:51:01 -0400

    Update Program.cs

--- csci-1302/Program.cs
+++ csci-1302/Program.cs
@@ -12,36 +12,7 @@ namespace csci_1302
             //Homework2.Run();
             //ArrayTests.Run();
             //ArrayTests2D.Run();
-
-            #region Lists
-
-            //GenericList<int> theList = new GenericList<int>();
-            //theList.AddHead(1);
-            //theList.AddHead(2);
-            //theList.AddHead(3);
-            //theList.AddTail(1);
-            //theList.AddTail(2);
-            //theList.AddTail(3);
-            GenericList<Person> theList = new GenericList<Person>();
-            theList.AddHead(new Person("Sam", "Smith", 55, 4));
-            theList.AddTail(new Person("Sally", "Smith", 35, 7));
-
-
-            foreach (var item in theList)
-            {
-                //if (item.Firstname == "Sam")
-                //{
-                //    Console.WriteLine("Found Sam");
-                //    break;
-                //}
-            }
-
-            Person foundVal = theList.Search(new Person("Sam", "Smith", 55, 4));
-
-            //int foundVal = theList.Search(99);
-            Console.WriteLine(foundVal);
-
-            #endregion
+            InheritanceTests.Run();
         }
     }
 }
