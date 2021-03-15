a1599f20e2e403c6897591717de0fa82d3514a99
Peter Mourfield <1678724+peter-mourfield@users.noreply.github.com>
2021-03-15 15:50:14 -0400

    Create Homework3.cs

---
+++ csci-1302/Homework3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace csci_1302
+{
+    public class Homework3
+    {
+        public static void Run()
+        {
+            // GenericList<int> aList = new GenericList<int>();
+            // GenericList<string> aList2 = new GenericList<string>();
+            // GenericList<Person> people = new GenericList<Person>();
+            GenericList<double> myList = new GenericList<double>();
+            // 1. Empty List
+            myList.RemoveLast();
+            if (myList.Count != 0)
+                Console.WriteLine("Error in test case 1");
+
+            // 2.  One Item
+            myList.AddHead(5);
+            myList.RemoveLast();
+            if (myList.Count != 0)
+                Console.WriteLine("Error in test case 2");
+
+            // 3. multiple items
+            myList.AddHead(5);
+            myList.AddHead(5);
+            myList.AddHead(5);
+            myList.RemoveLast();
+            if (myList.Count != 2)
+                Console.WriteLine("Error in test case 3");
+        }
+    }
+}
